4bf4466a886824330ae46eebc1f3644349a6257e
Anshul <ayushyadav6a@gmail.com>
2023-08-01 13:48:27 +0530

    New projects

---
+++ array.js
@@ -0,0 +1,11 @@
+const number = [];
+number[0] = 1;
+number[1] = 2
+number[3] = 2
+console.log(number[2]);
+
+const numbers = [1, 8, 5, 3, 7];
+numbers.sort((a, b) => {
+    return a = b;
+});
+console.log(numbers);
