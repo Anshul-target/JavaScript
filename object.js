26393782aa58a1d27a0e1af89c76f10ff7f24c55
Anshul <ayushyadav6a@gmail.com>
2023-08-05 10:59:43 +0530

    New Updates

--- object.js
+++ object.js
@@ -8,5 +8,28 @@ const child = function closures() {
     console.log(price)
     console.log(this)
 }
-parentFunction();
+// parentFunction();clear
+
 // child();
+
+// Destructuring
+const obj = {
+    firstname: "Anshul",
+    lastName: "Yadav",
+    "college Information": {
+        course: "Btech",
+        branch: "IT",
+        year: "2023"
+    },
+    age: "20"
+
+}
+
+const { firstname, ...newobj } = obj
+// console.log(firstname)
+// console.log(newobj)
+
+
+// Spread Operator
+const newobj1 = { ...obj, firstname: "Ayush" }
+console.log(newobj1);
